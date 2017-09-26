ee384f4f6cb0a39b6153af091909fdbb92f02b2a
josue rojas <josuerojas.rojas@gmail.com>
2017-09-26 18:04:34 -0400

    removed double imports

--- Josue/edwins-hw1-updated.py
+++ Josue/edwins-hw1-updated.py
@@ -4,8 +4,8 @@ Created on Sep 12, 2017
 @author: EDWIN
 this is an update version just made the perceptron Algorithm into a function to not repeat code
 '''
-import numpy as np
 from random import choice
+from numpy import array, dot, random
 import math
 from pylab import ylim
 import matplotlib.pyplot as plt
@@ -16,26 +16,26 @@ import matplotlib.pyplot as plt
 unit_step = lambda x: 0 if x < 0 else 1
 
 training_data_or = [
-    (np.array([0,0,1]), 0),
-    (np.array([0,1,1]), 1),
-    (np.array([1,0,1]), 1),
-    (np.array([1,1,1]), 1), ]
+    (array([0,0,1]), 0),
+    (array([0,1,1]), 1),
+    (array([1,0,1]), 1),
+    (array([1,1,1]), 1), ]
 
 training_data_xor = [
-    (np.array([0,0,1]), 0),
-    (np.array([0,1,1]), 1),
-    (np.array([1,0,1]), 1),
-    (np.array([1,1,1]), 0), ]
+    (array([0,0,1]), 0),
+    (array([0,1,1]), 1),
+    (array([1,0,1]), 1),
+    (array([1,1,1]), 0), ]
 
 training_data_and = [
-    (np.array([0,0,1]), 0),
-    (np.array([0,1,1]), 0),
-    (np.array([1,0,1]), 0),
-    (np.array([1,1,1]), 1), ]
+    (array([0,0,1]), 0),
+    (array([0,1,1]), 0),
+    (array([1,0,1]), 0),
+    (array([1,1,1]), 1), ]
 
 
 def percAlgo(training_data, gateName=''):
-    w = np.random.rand(3)
+    w = random.rand(3)
     print('\nStarting seed',w)
     errors = []
     errors2 = []
@@ -46,13 +46,13 @@ def percAlgo(training_data, gateName=''):
 
     for _ in range(steps):
         x, expected = choice(training_data)
-        result = np.dot(w, x)
+        result = dot(w, x)
         error = expected - unit_step(result)
         errors.append(error)
         w += bias * error * x
 
     for x, _ in training_data:
-        result = np.dot(x, w)
+        result = dot(x, w)
         print("{}: {} -> {}".format(x[:2], result, unit_step(result)))
 
     ylim([-1,1])
