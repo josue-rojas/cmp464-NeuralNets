1caa19a5d78dc25315e6fc9d7eb2c26643386bec
josue rojas <josuerojas.rojas@gmail.com>
2017-10-12 13:04:39 -0400

    updae gates and whole script

--- Josue/class.py
+++ Josue/class.py
@@ -7,10 +7,11 @@ Created on Oct 3, 2017
 
 import numpy as np
 from scipy.optimize import minimize# this is so can use their minimize
-X=np.array([0,0,1,0,0,1,1,1]).reshape(4,2)
-ORY = np.array([0,1,1,1]).reshape(4,1)
-ANDY = np.array([0,0,0,1]).reshape(4,1)
-XORY = np.array([0,1,1,0]).reshape(4,1)
+
+# X=np.array([0,0,1,0,0,1,1,1]).reshape(4,2) #moved it down (too many comments here)
+# ORY = np.array([0,1,1,1]).reshape(4,1)
+# ANDY = np.array([0,0,0,1]).reshape(4,1)
+# XORY = np.array([0,1,1,0]).reshape(4,1)
 #===============================================================================
 # The above is the basic inputs for truth tables
 # that we can use for XOR OR AND ETC
@@ -20,7 +21,7 @@ XORY = np.array([0,1,1,0]).reshape(4,1)
 # We will make a one layer net here where there will have to be two
 # weights and one bias.
 #===============================================================================
-W= np.random.randn(2,1)#right way for matrix mult
+# W= np.random.randn(2,1)#right way for matrix mult # I moved this down to the gateFunction method to better handle minimize guess
 #===============================================================================
 # W is the weight matrix which is 2X1 so that we can multiply
 # np.dot(X,W) and then add the bias to get an output,
@@ -38,29 +39,44 @@ W= np.random.randn(2,1)#right way for matrix mult
 # #scipy minimize expects the function and the three initial values for w0,w1 and b in a "list"
 # # that we have in the 2X1 array W and the scalar b.
 #===============================================================================
-b=1 # I am thinking of this as the initial value of b
-Y=ORY# this is output for the OR
+# b=1 # I am thinking of this as the initial value of b # this was moved to the gateFunction to handle better the minimize guess
+# Y=[]# this is output for the OR
 
 # given our inputs in X
 
-def cost(guess, Y=Y):
+def cost(guess):
+    inner = X.shape[1]
     b = guess[-1]
-    w = np.array(guess[:-1]).reshape(2,1)
+    w = np.array(guess[:-1]).reshape(inner,1)
     costArray= ((np.dot(X,w)+b)-Y)**2 # taking square here to eventually take sum of squares
     return costArray.sum()
 
 
-def gateFunction(Y=Y,gateName='OR'):
-    res = minimize(cost,[W[0,0],W[1,0],b],method='Nelder-Mead')
+def gateFunction(gateName='OR'):
+    inner = X.shape[1]
+    guess = np.random.randn(inner)
+    b = 1
+    guess = np.append(guess, b)
+    res = minimize(cost,list(guess),method='Nelder-Mead')
     print ("the w0, w1, and bias b you get are \n", res.x[0],res.x[1],res.x[2])
-    Wanswer = np.array([res.x[0], res.x[1]])
-    banswer = res.x[2]
+    Wanswer = np.array(res.x[:-1])
+    banswer = res.x[-1]
     Output = np.dot(X,Wanswer) + banswer
     print ('Output for ', gateName, ': ',Output)
 
-gateFunction(Y=ORY, gateName='OR')
-gateFunction(Y=ANDY,gateName='AND')
-gateFunction(Y=XORY, gateName='XOR')
+X = np.array([0,0,1,0,0,1,1,1]).reshape(4,2)
+Y = []
+
+Y = np.array([0,1,1,1]).reshape(4,1) # or y outputs
+gateFunction(gateName='OR')
+
+Y = np.array([0,0,0,1]).reshape(4,1) # and y outputs
+gateFunction(gateName='AND')
+
+# x with an extra bit should be [0,0,0] [0,0,1] [0,1,0] [1,0,0] [1,0,1] [1,1,0] [1,1,1] [0,1,1]
+X = np.array([0,0,0,0,0,1,0,1,0,1,0,0,1,0,1,1,1,0,1,1,1,0,1,1]).reshape(8,3)
+Y = np.array([0,1,1,1,1,1,0,1]).reshape(8,1) # xor y outputs (i think)
+gateFunction(gateName='XOR')
 
 
 #===============================================================================
